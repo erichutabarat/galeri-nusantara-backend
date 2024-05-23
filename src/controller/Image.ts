aa369ad51d60e07a572accd661a652a6f02040fd
Eric Hutabarat <76461464+erichutabarat@users.noreply.github.com>
2024-05-23 12:03:32 +0700

    Feature: Image (Update)
    Update image feature:
    - Upload image to cdn

--- src/controller/Image.ts
+++ src/controller/Image.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import responseMiddleWare from "../middleware/response-middleware";
 import imageModels from "../models/image-models"
+import Uploader from "../utils/uploader";
+import fs from "fs";
 
 const ImageController = {
     async getAll(req: Request, res: Response){
@@ -29,6 +31,41 @@ const ImageController = {
         const data = await imageModels.getImageByBudayaId(newId);
         const response = responseMiddleWare("Success", "Success get data!", data);
         return res.status(200).json(response);
+    },
+    async uploadImage(req: Request, res: Response) {
+        if(req.file){
+            const { budayaid } = req.body;
+            if(!budayaid){
+                return res.json({
+                    message: 'Please input budaya id!'
+                });
+            }
+            const budayaId = parseInt(budayaid);
+            const paths = req.file.path;
+            const uploading = await Uploader(paths);
+            const creating = await imageModels.createImage(uploading, budayaId);
+            fs.unlink(paths, (error) => {
+                if(error){
+                    console.error(error);
+                }
+            });
+            if(creating){
+                return res.json({
+                    message: 'upload success',
+                    location: uploading
+                });
+            }
+            else{
+                return res.json({
+                    message: 'upload failed'
+                });
+            }
+        }
+        else{
+            return res.json({
+                message: 'upload failed'
+            });
+        }
     }
 };
 
--- src/routes/images-routes.ts
+++ src/routes/images-routes.ts
@@ -1,10 +1,6 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import ImageController from "../controller/Image";
 import UploadMiddleWare from "../middleware/upload-middleware";
-import Uploader from "../utils/uploader";
-import imageModels from "../models/image-models";
-import fs from "fs";
-import { error } from "console";
 
 const ImagesRoutes = Router();
 
@@ -12,40 +8,6 @@ ImagesRoutes.get('/', ImageController.getAll);
 ImagesRoutes.get('/:id', ImageController.getById);
 ImagesRoutes.get('/budaya/:id', ImageController.getByBudayaId);
 
-ImagesRoutes.post('/upload', UploadMiddleWare.single("images"), async (req: Request, res: Response) => {
-    if(req.file){
-        const { budayaid } = req.body;
-        if(!budayaid){
-            return res.json({
-                message: 'Please input budaya id!'
-            });
-        }
-        const budayaId = parseInt(budayaid);
-        const paths = req.file.path;
-        const uploading = await Uploader(paths);
-        const creating = await imageModels.createImage(uploading, budayaId);
-        fs.unlink(paths, (error) => {
-            if(error){
-                console.error(error);
-            }
-        });
-        if(creating){
-            return res.json({
-                message: 'upload success',
-                location: uploading
-            });
-        }
-        else{
-            return res.json({
-                message: 'upload failed'
-            });
-        }
-    }
-    else{
-        return res.json({
-            message: 'upload failed'
-        });
-    }
-});
+ImagesRoutes.post('/upload', UploadMiddleWare.single("images"), ImageController.uploadImage);
 
 export default ImagesRoutes;
