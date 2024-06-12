161cbd2729b8b86eb797cb2f8651ad94a4220a98
Eric Hutabarat <76461464+erichutabarat@users.noreply.github.com>
2024-06-12 12:34:44 +0700

    Update budaya-routes.ts

--- src/routes/budaya-routes.ts
+++ src/routes/budaya-routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import BudayaController from "../controller/Budaya";
-import Test from "../controller/Test";
 
 const BudayaRoutes = Router();
 
 BudayaRoutes.get('/', BudayaController.getAll);
 BudayaRoutes.delete('/', BudayaController.deleteBudaya);
 BudayaRoutes.get('/:id', BudayaController.getById);
-BudayaRoutes.post('/:id/update', BudayaController.updateBudaya);
+BudayaRoutes.put('/:id', BudayaController.updateBudaya);
 BudayaRoutes.post('/', BudayaController.createBudaya);
 
 export default BudayaRoutes;
