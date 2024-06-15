e36aa312ecd86c755ad199f779b3900aca422283
Eric Hutabarat <76461464+erichutabarat@users.noreply.github.com>
2024-06-15 14:49:06 +0700

    Update upload-middleware.ts

--- src/middleware/upload-middleware.ts
+++ src/middleware/upload-middleware.ts
@@ -2,9 +2,7 @@ import multer from "multer";
 import path
  from "path";
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, __dirname);
-    },
+    destination: require.main?.path + "/" +" public/images/",
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
