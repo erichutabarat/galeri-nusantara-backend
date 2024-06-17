fe65215cd2660d235c4b232c7c081de0cbf2fb65
Eric Hutabarat <76461464+erichutabarat@users.noreply.github.com>
2024-06-17 13:44:05 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import server from "./application/server";
-import https from "https";
+// import https from "https";
 import fs from "fs";
 
 const Apps = server;
 const port = process.env.PORT || 443;
 
-// Paths to the SSL certificate and key files
-const sslOptions = {
-    key: fs.readFileSync('/home/ec2-user/galeri-nusantara-backend/ssl/selfsigned.key'),
-    cert: fs.readFileSync('/home/ec2-user/galeri-nusantara-backend/ssl/selfsigned.crt')
-};
+// // Paths to the SSL certificate and key files
+// const sslOptions = {
+//     key: fs.readFileSync('/home/ec2-user/galeri-nusantara-backend/ssl/selfsigned.key'),
+//     cert: fs.readFileSync('/home/ec2-user/galeri-nusantara-backend/ssl/selfsigned.crt')
+// };
 
-https.createServer(sslOptions, Apps).listen(port, () => {
-    console.log(`Server running on https://0.0.0.0:${port}`);
-});
+Apps.listen(port, () => {
+    console.log(`Server run on ports ${port}`);
+})
