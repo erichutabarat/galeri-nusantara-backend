f9c92c5359d216fe8f75f8e56fbfc59e392126dd
Eric Hutabarat <76461464+erichutabarat@users.noreply.github.com>
2024-06-17 12:37:24 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import server from "./application/server";
+import https from "https";
+import fs from "fs";
 
 const Apps = server;
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 443;
 
-Apps.listen(port, () => {
-    console.log(`Server running on http://127.0.0.1:${port}`);
-})
+// Paths to the SSL certificate and key files
+const sslOptions = {
+    key: fs.readFileSync('../ssl/selfsigned.key'),
+    cert: fs.readFileSync('../ssl/selfsigned.crt')
+};
+
+https.createServer(sslOptions, Apps).listen(port, () => {
+    console.log(`Server running on https://127.0.0.1:${port}`);
+});
